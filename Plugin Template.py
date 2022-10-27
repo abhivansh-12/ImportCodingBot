7a2a27f78cc6bce2fc57563c8f3070b89b635385
SysWOW64UwU <81893400+SysWOW64UwU@users.noreply.github.com>
2022-10-27 14:41:39 +0530

    Update Plugin Template.py

--- Plugin Template.py
+++ Plugin Template.py
@@ -9,12 +9,12 @@ load_dotenv()
 
 preFix = "?" # Change this character for a custom command prefix, leave empty to use the master prefix defined in .env, the fallback prefix is  TG's default "/" prefix
 cmds = ["mycmd", F"mycmd{os.getenv('BOT_UNAME')}"] # Change the command list accordingly
-HELP = F"{preFix}{cmds[0]} - Short info about mycommand" # Provide brief and meaningful help information about what the command is for
+HELP = F"{preFix if preFix else os.getenv('MASTER_PREFIX', '/')}{cmds[0]} - Short info about mycommand" # Provide brief and meaningful help information about what the command is for
 
 '''-------------------------------'''
 
 
-command = lambda cmd: filters.command(cmd, prefixes = preFix if preFix else os.getenv("MASTER_PREFIX", "/")
+command = lambda cmd: filters.command(cmd, prefixes = preFix if preFix else os.getenv('MASTER_PREFIX', '/'))
 
 @Client.on_message(command(cmds))
 async def FunctionName_SameAs_FileName(app, msg): # The function name will be myFunc if the file name is myFunc.py, not necessarily but try to stick to this convention
