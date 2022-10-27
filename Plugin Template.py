07ef33803da20ea977da7c6fc03343fcccdc889d
SysWOW64UwU <81893400+SysWOW64UwU@users.noreply.github.com>
2022-10-27 13:14:03 +0530

    Update Plugin Template.py

--- Plugin Template.py
+++ Plugin Template.py
@@ -1,11 +1,14 @@
 # All the imports on top, just for the sake of convention, not mandatory
 from pyrogram import Client, filters
+from dotenv import load_dotenv
+import os
 
 
+load_dotenv()
 '''===========EDITABLES==========='''
 
 preFix = "?" # Change this character for a custom command prefix
-cmds = ["mycmd", "mycmd@botusername"] # Change the command list accordingly
+cmds = ["mycmd", F"mycmd{os.getenv("BOT_UNAME")}"] # Change the command list accordingly
 HELP = F"{preFix}{cmds[0]} - Short info about mycommand" # Provide brief and meaningful help information about what the command is for
 
 '''-------------------------------'''
